a43f3abd438bd1e5cfbd5b0753108bf51dded69f
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-07-12 10:20:01 -0700

    Update commits.go

--- commits/commits.go
+++ commits/commits.go
@@ -1,16 +1,31 @@
 package commits
 
 import (
-	"util"
 	"github.com/google/go-github/github"
+	"util"
 )
 
 // given a list of commits for a repo opened by a user, a response body, and a map of commits,
 // adds mappings for the times each commit was opened
-func GetCommitTimes(commitsList []*github.RepositoryCommit, resp *github.Response, mCommits map[int]int) {
+func GetCommitList(commitsList []*github.RepositoryCommit) []*github.RepositoryCommit {
+	m := make(map[string]*github.RepositoryCommit)
+	var list []*github.RepositoryCommit
 	for _, commit := range commitsList {
+		id := commit.GetSHA()
+		if _, ok := m[id]; !ok {
+			m[id] = commit
+		} 
+	}
+	for _, v := range m {
+		list = append(list, v)
+	}
+	return list
+}
+
+func GetCommitTimes(list []*github.RepositoryCommit, mCommits map[int]int) {
+	for _, commit := range list {
 		author := commit.Commit.GetAuthor()
-		time := author.GetDate().Format("2006-01-02")
+		time := author.GetDate().Format("2006-01-02T15:04:05Z07:00")
 		util.AddToMap(mCommits, time)
 	}
 }
