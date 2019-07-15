a7a224adcb0de08b6d1fd8246e8202e9ef41ac4d
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-07-15 11:42:53 -0700

    Update issues.go

--- issues/issues.go
+++ issues/issues.go
@@ -1,37 +1,21 @@
 package issues
 
 import (
-	"context"
 	"util"
 	"github.com/google/go-github/github"
 )
 
-// given a list of issues created by a user and a record of issues for the user,
-// adds a mapping for the time created
-func GetIssueCreatedTimes(issueListCreator []*github.Issue, mIssues map[int]int) {
-	var time string
-	for _, issue := range issueListCreator {
-		time = issue.GetCreatedAt().Format("2006-01-02T15:04:05Z07:00")
-		util.AddToMap(mIssues, time)
-	}
+//
+type Issues interface {
+	GetIssueCreatedTimes(l []*github.Issue, mIssues map[int]int)
 }
 
-// given a context, a client, a repo's owner, a repo's name, a user's login, a list of issues
-// for that repo, and a record of issues for the user, adds a mapping for events where
-// issue was assigned to user or user was mentioned in issue
-func GetIssueEventTimes(ctx context.Context, client *github.Client, repoOwner string, repoName string,
-						username string, issueList []*github.Issue, mIssues map[int]int) {
+// given issues created by a user and a record of issues for the user,
+// adds a mapping for the time created
+func GetIssueCreatedTimes(list []*github.Issue, mIssues map[int]int) {
 	var time string
-	for _, issue := range issueList {
-		num := issue.GetNumber()
-		events, _, err := client.Issues.ListIssueEvents(ctx, repoOwner, repoName, num, nil)
-		util.ThrowError(err)
-		for _, event := range events {
-			if *event.Event == "assigned" && event.Assignee.GetLogin() == username ||
-				*event.Event == "mentioned" && event.Actor.GetLogin() == username {
-				time = event.GetCreatedAt().Format("2006-01-02T15:04:05Z07:00")
-				util.AddToMap(mIssues, time)
-			}
-		}
+	for _, issue := range list {
+		time = issue.GetCreatedAt().Format("2006-01-02T15:04:05Z07:00")
+		util.AddToMap(mIssues, time)
 	}
 }
