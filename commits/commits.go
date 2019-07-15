6edf3b6d2852732da58601f822f7128b3660c318
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-07-15 11:42:21 -0700

    Update commits.go

--- commits/commits.go
+++ commits/commits.go
@@ -5,23 +5,13 @@ import (
 	"util"
 )
 
-// given a list of commits for a repo opened by a user, a response body, and a map of commits,
-// adds mappings for the times each commit was opened
-func GetCommitList(commitsList []*github.RepositoryCommit) []*github.RepositoryCommit {
-	m := make(map[string]*github.RepositoryCommit)
-	var list []*github.RepositoryCommit
-	for _, commit := range commitsList {
-		id := commit.GetSHA()
-		if _, ok := m[id]; !ok {
-			m[id] = commit
-		} 
-	}
-	for _, v := range m {
-		list = append(list, v)
-	}
-	return list
+//
+type Commits interface {
+	GetCommitTimes(list []*github.RepositoryCommit, mCommits map[int]int)
 }
 
+// given commits for a repository, adds mapping between a commit and the time
+// the commit was created
 func GetCommitTimes(list []*github.RepositoryCommit, mCommits map[int]int) {
 	for _, commit := range list {
 		author := commit.Commit.GetAuthor()
