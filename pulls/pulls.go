5b1d0db5ac36797072cebee9263ee0a809cde6b5
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-07-15 11:44:07 -0700

    Update pulls.go

--- pulls/pulls.go
+++ pulls/pulls.go
@@ -6,21 +6,26 @@ import (
 	"github.com/google/go-github/github"
 )
 
-// given a context, a client, a repo's owner, a repo's name, a user's login, a list of pull requests for a repo,
-// and a record of pull requests for a user, adds a mapping for the times pull requests reviews
-// were assigned to user
+type Pulls interface {
+	GetPullsReviewRequestTimes(ctx context.Context, list []*github.PullRequest, mPulls map[int]int)
+}
+
+// given a context, a client, a repo's owner, a repo's name, a user's login, pull requests for a repo,
+// and a record of pull requests for a user, adds a mapping for times the user reviewed a pull request
 func GetPullsReviewRequestTimes(ctx context.Context, client *github.Client, repoOwner string, repoName string, username string,
-								pullsList []*github.PullRequest, mPulls map[int]int) {
-	 var time string
-	 for _, pull := range pullsList {
+								list []*github.PullRequest, mPulls map[int]int) {
+	var time string
+	for _, pull := range list {
 		num := pull.GetNumber()
 		reviews, _, err := client.PullRequests.ListReviews(ctx, repoOwner, repoName, num, nil)
-		util.ThrowError(err)
+		if err != nil {
+			return 
+		}
 		for _, review := range reviews {
 			if review.GetUser().GetLogin() == username {
 				time = review.GetSubmittedAt().Format("2006-01-02T15:04:05Z07:00")
 				util.AddToMap(mPulls, time)
 			}
 		}
-	}	
+	}
 }
