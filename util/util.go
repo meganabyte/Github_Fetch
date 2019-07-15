993e04a8646ffc8d2a0106a39d2736d77d1a13b7
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-07-15 11:44:49 -0700

    Update util.go

--- util/util.go
+++ util/util.go
@@ -1,18 +1,27 @@
 package util
 
 import (
-	"fmt"
-	"os"
 	"time"
+	"math"
 )
+
+type Util interface {
+	timeDiffInDays(day string)
+	AddToMap(aMap map[int]int, time string)
+	ComputeContr(mIssues map[int]int, mPulls map[int]int, mCommits map[int]int)
+	getContr(m map[int]int)
+}
+
+const numDays = 366
+type contrArray = [numDays]int
 
 // returns the difference in days between today and the given day
 func timeDiffInDays(day string) int {
 	today := time.Now()
-	layout := "2006-01-02"
+	layout := "2006-01-02T15:04:05Z07:00" 
 	t, _ := time.Parse(layout, day)
 	daysDiff := (today.Sub(t).Hours()) / 24
-	return int(daysDiff)
+	return int(math.Round(daysDiff))
 }
 
 // given a map and time, computes the index and adds mapping between an index and count
@@ -30,30 +39,22 @@ func AddToMap(aMap map[int]int, time string) {
 }
 
 // given issues, pulls, and commits, returns a record of all user contributions over the past year
-func ComputeContr(mIssues map[int]int, mPulls map[int]int, mCommits map[int]int) [366]int {
-	var result [366]int
+func ComputeContr(mIssues map[int]int, mPulls map[int]int, mCommits map[int]int) contrArray {
+	var result contrArray
 	arrIssues := getContr(mIssues)
 	arrPulls := getContr(mPulls)
 	arrCommits := getContr(mCommits)
-	for i := 0; i < 366; i++ {
+	for i := 0; i < numDays; i++ {
 		result[i] = arrIssues[i] + arrPulls[i] + arrCommits[i]
 	}
 	return result
 }
 
 // given a map, returns a record of a subset of user contributions over the past year
-func getContr(m map[int]int) [366]int {
-	var arr [366]int
+func getContr(m map[int]int) contrArray {
+	var arr contrArray
 	for key, value := range m {
 		arr[key] = value
 	}
 	return arr
-}
-
-// given an error, prints error and throws error if error is not nil
-func ThrowError(err error) {
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
 }
