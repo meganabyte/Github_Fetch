0cd990715b72ac38655db90b397ea9a79fef13b1
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-07-15 11:43:34 -0700

    Update paginate.go

--- paginate/paginate.go
+++ paginate/paginate.go
@@ -3,74 +3,71 @@ package paginate
 import (
 	"context"
 	"time"
-	"util"
 	"github.com/google/go-github/github"
 )
 
+type Paginate interface {
+	IssuesCreated(ctx context.Context, client *github.Client, repoOwner string, repoName string,
+				  username string, yearAgo time.Time)
+	Pulls(ctx context.Context, client *github.Client, repoOwner string, repoName string,
+		  username string, yearAgo time.Time) ([]*github.PullRequest, error)
+	Commits(ctx context.Context, client *github.Client, repoOwner string, repoName string, username string, yearAgo time.Time, 
+			repo *github.Repository)
+}
+
 // given a context, a client, a repo's owner, a repo's name, a user's login, and the time a year ago,
-// returns a list of all issues created by the user in that repo
+// returns all issues created by the user in that repo
 func IssuesCreated(ctx context.Context, client *github.Client, repoOwner string, repoName string,
-				   username string, yearAgo time.Time) []*github.Issue {
-	var issueListCreator []*github.Issue
+				   username string, yearAgo time.Time) ([]*github.Issue, error) {
+	var l []*github.Issue
 	opt := &github.IssueListByRepoOptions{Creator: username, Since: yearAgo, State: "all", ListOptions: github.ListOptions{PerPage: 30}}
 	for {
 		list, resp, err := client.Issues.ListByRepo(ctx, repoOwner, repoName, opt)
-		util.ThrowError(err)
-		issueListCreator = append(issueListCreator, list...)
+		if err != nil {
+			return nil, err
+		}
+		l = append(l, list...)
 		if resp.NextPage == 0 {
 			break
 		}
 		opt.Page = resp.NextPage
 	}
-	return issueListCreator
-}
-
-// given a context, a client, a repo's owner, a repo's name, and a user's login, and the time a year ago,
-// returns a list of all issues in that repo
-func IssueEvents(ctx context.Context, client *github.Client, repoOwner string, repoName string,
-				 username string, yearAgo time.Time) []*github.Issue {
-	var issueList []*github.Issue
-	opt := &github.IssueListByRepoOptions{Since: yearAgo, State: "all", ListOptions: github.ListOptions{PerPage: 30}}
-	for {
-		list, resp, err := client.Issues.ListByRepo(ctx, repoOwner, repoName, opt)
-		util.ThrowError(err)
-		issueList = append(issueList, list...)
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
-	}
-	return issueList
+	return l, nil
 }
 
 // given a context, a client, a repo's owner, a repo's name, a user's login, and the time a year ago,
-// returns a list of all pull requests for that repo
+// returns all pull requests for that repo
 func Pulls(ctx context.Context, client *github.Client, repoOwner string, repoName string,
-		   username string, yearAgo time.Time) []*github.PullRequest {
-	var pullsList []*github.PullRequest
+		   username string, yearAgo time.Time) ([]*github.PullRequest, error) {
+	var l []*github.PullRequest
 	opt := &github.PullRequestListOptions{State: "all", ListOptions: github.ListOptions{PerPage: 30}}
 	for {
 		list, resp, err := client.PullRequests.List(ctx, repoOwner, repoName, opt)
-		util.ThrowError(err)
-		pullsList = append(pullsList, list...)
-		if resp.NextPage == 0 {
+		if err != nil {
+			return nil, err
+		}
+		l = append(l, list...)
+		if resp.NextPage == 0 || opt.Page == 10 {
 			break
 		}
 		opt.Page = resp.NextPage
 	}
-	return pullsList
+	return l, nil
 }
 
 // given a context, a client, a repo's owner, a repo's name, a user's login, and the time a year ago,
-// and a repo, a returns a list of all commits for that repo
-func Commits(ctx context.Context, client *github.Client, repoOwner string, repoName string, username string,
-			 yearAgo time.Time, repo *github.Repository) []*github.RepositoryCommit {
+// and a repo, returns all commits for that repo
+func Commits(ctx context.Context, client *github.Client, repoOwner string, repoName string, username string, yearAgo time.Time, 
+			 repo *github.Repository) ([]*github.RepositoryCommit, error) {
 	var commitsList []*github.RepositoryCommit
-	opt := &github.CommitsListOptions{Author: username, Since: yearAgo, ListOptions: github.ListOptions{PerPage: 30}}
-	if repo.GetSize() != 0 {
+	// var uniqueList []*github.RepositoryCommit
+		opt := &github.CommitsListOptions{SHA: "master", Author: username, Since: yearAgo, ListOptions: github.ListOptions{PerPage: 30}}
+		if repo.GetSize() != 0 {
 		for {
 			list, resp, err := client.Repositories.ListCommits(ctx, repoOwner, repoName, opt)
-			util.ThrowError(err)
+			if err != nil {
+				return nil, err
+			}
 			commitsList = append(commitsList, list...)
 			if resp.NextPage == 0 {
 				break
@@ -78,5 +75,5 @@ func Commits(ctx context.Context, client *github.Client, repoOwner string, repoN
 			opt.Page = resp.NextPage
 		}
 	}
-	return commitsList
+	return commitsList, nil
 }
